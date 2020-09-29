1f8f0505a9154ff8df7ea60ec8db95191eea0804
Poltery <69917012+Poltery@users.noreply.github.com>
2020-09-29 07:36:16 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 """
-Title:
-Creators:
-Description:
+Title: Prison Peril
+Creators: Michael W. and Jason B.
+Description: The guy is doing stuff! oh crap!
 """
+Merek = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . f f f f f f f f . . . .
+    . . . f 4 4 e 4 4 4 4 1 f f . .
+    . . f f 4 4 4 4 4 4 4 f 4 f . .
+    . . f 4 1 4 4 4 4 4 e 4 4 f . .
+    . f e 4 f 4 4 4 4 4 4 4 f . f .
+    . f f 4 4 4 f f f f 4 f . . f .
+    . f . f f 4 e 4 4 4 f f . . f .
+    . f . . . f f f f f . . . . f .
+    . f . . f f f . f f f . . f . f
+    f . f f f f f . f f f f . . . .
+    . . . f f f . . . f f f . . . .
+    . . . f f . . . . f f f . . . .
+    . . . f . . . . . . . f . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+scene.set_background_color(3)
+info.set_life(3)
+tiles.set_tilemap(tilemap(""" """))
--- main.ts
+++ main.ts
@@ -1,6 +1,27 @@
 /** 
-Title:
-Creators:
-Description:
+Title: Prison Peril
+Creators: Michael W. and Jason B.
+Description: The guy is doing stuff! oh crap!
 
  */
+let Merek = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . f f f f f f f f . . . .
+    . . . f 4 4 e 4 4 4 4 1 f f . .
+    . . f f 4 4 4 4 4 4 4 f 4 f . .
+    . . f 4 1 4 4 4 4 4 e 4 4 f . .
+    . f e 4 f 4 4 4 4 4 4 4 f . f .
+    . f f 4 4 4 f f f f 4 f . . f .
+    . f . f f 4 e 4 4 4 f f . . f .
+    . f . . . f f f f f . . . . f .
+    . f . . f f f . f f f . . f . f
+    f . f f f f f . f f f f . . . .
+    . . . f f f . . . f f f . . . .
+    . . . f f . . . . f f f . . . .
+    . . . f . . . . . . . f . . . .
+    . . . . . . . . . . . . . . . .
+`)
+scene.setBackgroundColor(3)
+info.setLife(3)
+tiles.setTilemap(tilemap` `)
